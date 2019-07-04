59d7c89cf3f7d76a087fce367ef9084d20b193d5
1874522282 <1874522282@qq.com>
2019-07-04 19:59:20 +0800

    updated

--- ubuntu_sim_common_deps.sh
+++ ubuntu_sim_common_deps.sh
@@ -67,20 +67,4 @@ else
 fi
 
 
-# GNU Arm Embedded Toolchain: 7-2017-q4-major December 18, 2017
-gcc_dir=$HOME/gcc-arm-none-eabi-7-2017-q4-major
-echo "Installing GCC to: $gcc_dir"
-if [ -d "$gcc_dir" ]
-then
-    echo " GCC already installed."
-else
-    pushd .
-    cd ~    
-    wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-7-2017-q4-major-linux.tar.bz2
-    tar -jxf gcc-arm-none-eabi-7-2017-q4-major-linux.tar.bz2
-    exportline="export PATH=$HOME/gcc-arm-none-eabi-7-2017-q4-major/bin:\$PATH"
-    if grep -Fxq "$exportline" ~/.profile; then echo " GCC path already set." ; else echo $exportline >> ~/.profile; fi
-    . ~/.profile
-    popd
-fi
 
